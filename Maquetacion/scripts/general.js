5428dae41ba1951fb99e7aa2624c42cb28ba80d1
Juan José Díaz Marrero <juanjoseg04@gmail.com>
2020-05-01 23:35:04 +0100

    Corrigiendo Ids únicos mediante javascript

--- Maquetacion/scripts/general.js
+++ Maquetacion/scripts/general.js
@@ -4,7 +4,9 @@ window.addEventListener("load" ,function(){
     var articles =  this.document.getElementById("articles");
     for(var i=0; i<5 ; i++){
         var clonate =  template.cloneNode(true);
+        clonate.removeAttribute('id');
         var h2 = clonate.getElementsByTagName("h2")[0];
+        this.console.log(clonate);
         h2.innerHTML = h2.textContent + ' ' + i;
         articles.appendChild(clonate);
     }

6e6a75102e018cd4b9c159f0c4921ce1c6f9c352
Juan José Díaz Marrero <juanjoseg04@gmail.com>
2020-05-20 23:28:46 +0100

    Añadiendo primer endpoint de prueba en app.js

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,20 @@ app.get('/datos-curso', function(req, res){
 
    return  res.status(200).send({
        cursos:'Master en Frameworks JS',
-       autor: 'Juan Jose',
+       autor: 'Juan Jose'
    });
 });
+app.post('/datos-curso', function(req, res){
+
+    var param = req.body.hola;
+    return res.status(200).send({
+        cursos:'Master en Frameworks JS',
+        autor: 'Juan Jose',
+        hola: param
+    });
+ });
+ 
+
 
 // Exportación del mmódulo
 module.exports = app;

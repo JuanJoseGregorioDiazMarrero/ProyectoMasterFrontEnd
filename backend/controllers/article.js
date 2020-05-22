356ad6df31b9fef58244f0b51873466eb565206c
Juan José Díaz Marrero <juanjoseg04@gmail.com>
2020-05-22 01:29:32 +0100

    Implementando controlador getArticles

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -1,11 +1,14 @@
 'use strict'
 var  validator = require('validator');
 var Article = require('../models/article');
+const msgSuccess = 'Éxito';
+const msgError = 'Error';
 var controller = {
 
     datosCurso: (req, res) => {
         var param = req.body.param;
             return  res.status(200).send({
+                status: msgSuccess,
                 cursos:'Master en Frameworks JS',
                 autor: 'Juan Jose',
                 param: param
@@ -13,57 +16,72 @@ var controller = {
     },
     test : (req, res) => {
             return res.status(200).send({
+                status: msgSuccess,
                 message:'Respuesta Controllador de Artículos'
             });
     },
     save: (req, res) => {
-         
-          
-            // Validar Datos
-            try {
-                var params = req.body;
-                var hasContent = !validator.isEmpty(params.content);
-                var hasTitle = !validator.isEmpty(params.title);
-                if(hasContent && hasTitle){
-                    // Crear Objeto a guardar
-                    var article = new Article();
-                    // Asignar  valores
-                    article.title = params.title;
-                    article.content = params.content;
-                    article.image = null;
-                    article.save((error, articleStored)=> {
-                        if (error || !articleStored) {
-                            return res.status(404).send({
-                                status:'error',
-                                message: 'El artículo no se ha podido guardar '
-                            });
-                        }
-                        return res.status(200).send({
-                            status:'success',
-                            article : articleStored
+        // Validar Datos
+        try {
+        var params = req.body;
+        var hasContent = !validator.isEmpty(params.content);
+        var hasTitle = !validator.isEmpty(params.title);
+            if(hasContent && hasTitle){
+                // Crear Objeto a guardar
+                var article = new Article();
+                // Asignar  valores
+                article.title = params.title;
+                article.content = params.content;
+                article.image = null;
+                article.save((error, articleStored)=> {
+                    if (error || !articleStored) {
+                        return res.status(404).send({
+                            status: msgError,
+                            message: 'El artículo no se ha podido guardar '
                         });
-                    })
-
-                } else {
-                    return res.status(400).send({
-                        message: 'Parámetros incompletos o malformados'
+                    }
+                    return res.status(201).send({
+                        status: msgSuccess,
+                        article : articleStored
                     });
-                }
-            } catch (error) {
-                return res.status(500).send({
-                    status: "Error",
-                    message: JSON.stringify(error)
+                })
+
+            } else {
+                return res.status(400).send({
+                    status: msgError,
+                    message: 'Parámetros incompletos o malformados'
                 });
             }
+        } catch (error) {
+            return res.status(500).send({
+                status: msgError,
+                message: JSON.stringify(error)
+            });
+        }  
+    },
+    getAllArticles : (req, res ) => {
 
-        
+        Article.find({}).sort('-_id').exec((error, articles) => {
+            if (error) {
+                return res.status(500).send({
+                    status: msgError,
+                    message: error.toString()
+                });
+            }
+            if (!articles || articles.length == 0){
+                return res.status(204).send({
+                    status: msgSuccess,
+                    message: 'No se encontraron artículos'
+                });
+            }
+            return res.status(200).send({
+                status: msgSuccess,
+                articles
+            });
 
-        
+        })
 
-            // Almacenar objeto
-
-            // Responder 
-         
+    
     }
 };
 
